1ae22b9041b4c3e22fd43cc275ee513541450612
Locus <locuswin@gmail.com>
2025-04-06 17:51:54 -0400

    Adding menu items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({required this.title, required this.subTitle, required this.link, required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Widgets',
+    subTitle: 'Basic widgets',
+    link: '/widgets',
+    icon: Icons.widgets
+  ),
+  MenuItem(
+    title: 'Buttons',
+    subTitle: 'Buttons widgets',
+    link: '/buttons',
+    icon: Icons.touch_app,
+  ),
+  MenuItem(
+    title: 'Cards',
+    subTitle: 'Cards widgets',
+    link: '/cards',
+    icon: Icons.credit_card,
+  ),
+  MenuItem(
+    title: 'Dialogs',
+    subTitle: 'Dialogs widgets',
+    link: '/dialogs',
+    icon: Icons.chat_bubble,
+  ),
+];
